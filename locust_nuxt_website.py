8950360fd3986c921ca5712b737ff1971e6ccf01
pghorpade <pghorpade@library.ucla.edu>
2023-02-13 08:55:58 -0800

    initial commit

---
+++ locust_nuxt_website.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import random
+
+from locust import HttpUser, TaskSet, task
+from pyquery import PyQuery
+
+class RandomSitemapWalk(TaskSet):
+    def on_start(self):
+        r = self.client.get("/sitemap.xml")
+        pq = PyQuery(r.content, parser='html')
+        self.sitemap_links = []
+        for loc in pq.find('loc'):
+            self.sitemap_links.append(PyQuery(loc).text())
+
+    @task(10)
+    def load_page(self):
+        url = random.choice(self.sitemap_links)
+        r = self.client.get(url)
+
+class AwesomeUser(HttpUser):
+    tasks = [RandomSitemapWalk]
+    host = "https://uclalibrary.library.ucla.edu/"
+    
+    min_wait = 1  * 1000
+    max_wait = 10 * 1000
